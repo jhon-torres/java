ff77390cb98e6a63b41e5d5696e05869fb7202f5
Jhon Torres <38541491+jhon-torres@users.noreply.github.com>
2023-12-07 16:40:52 -0500

    métodos de la clase Calculadora

--- Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -5,12 +5,24 @@ public class TestCalculadora {
 	public static void main(String[] args) {
 		Calculadora calcu = new Calculadora();
 		int resultado;
+		double total;
 		
 		resultado = calcu.sumar(4, 5);
-		System.out.println("resultado: "+resultado);
+		System.out.println("Resultado suma: "+resultado);
 		
 		resultado = calcu.restar(9, 6);
-		System.out.println("resultado: "+resultado);
+		System.out.println("Resultado resta: "+resultado);
+		
+		total = calcu.multiplicar(10, 5);
+		System.out.println("Resultado multiplicación: "+total);
+		
+		total = calcu.dividir(10, 2);
+		System.out.println("Resultado división: "+total);
+		
+		total = calcu.promediar(10, 8, 9);
+		System.out.println("Resultado promediar: "+total);
+		
+		calcu.mostrarResultados();
 	}
 
 }
